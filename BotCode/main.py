6a867515b188d24a592b1fc2b7f466117a805229
V-Proskurin <83184433+V-Proskurin@users.noreply.github.com>
2022-11-08 13:46:44 +0300

    Добавил класс юзер и кэширование согласно видео https://www.youtube.com/watch?v=vOD57tVnCCM

--- BotCode/main.py
+++ BotCode/main.py
@@ -9,6 +9,14 @@ bot = telebot.TeleBot(token)
 # то удаляем установку вначале pip uninstall telebot , а затем pip uninstall pytelegrambotapi
 # и заново пишем pip install pytelegrambotapi - тогда файл запускается и все работает.
 
+#сохраняем данные пользователя вначале в программе
+user_dict = {}
+
+class User:
+    def __init__(self, first_name):
+        self.first_name = first_name
+        self.last_name = ''
+
 # Эта клавиатура появляется при старте. Сюда надо добавить кнопку для авторизации на сайте, разобраться как работает
 # https://t.me/x1test1Bot вот здесь описание
 
@@ -50,6 +58,12 @@ def answer(webAppMes):
     bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}")
     # отправляем сообщение в ответ на отправку данных из веб-приложения
 
+######################База данных wordpress#####################
+
+
+
+#Создаем переменные для сбора данных.
+
 # это наша старая команда, если /button отправили в чат, то появляется вопрос Выберите что вам надо и при нажатии кнопки возвращается текст Жми
 # @bot.message_handler(commands=['button'])
 # def button_message(message):
@@ -71,5 +85,14 @@ def answer(webAppMes):
 #         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 
+# Это какое-то кэширование. Записывает в файл. Возможно надо чистить, а то завалит память мусором.
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+bot.enable_save_next_step_handlers(delay=2)
+
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+bot.load_next_step_handlers()
 
 bot.infinity_polling()
